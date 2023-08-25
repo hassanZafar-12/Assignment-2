04ca9ca7ce25283293074425e3e5d57958d6d6a3
hassanZafar-12 <201370139@gift.edu.pk>
2023-08-25 22:57:53 +0500

    updated app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hassan Zafar</h1>
+      <h1> Hassan Zafar </h1>
       <h2>201370139</h2>
     </div>
   );
